92d8307ce2e5b84ef3f8b578d02b46dd6ca81b89
EmilienCombaret <89770414+EmilienCombaret@users.noreply.github.com>
2021-11-16 23:25:38 +0100

    Update récursivité.ml

--- algo/récursivité.ml
+++ algo/récursivité.ml
@@ -52,3 +52,33 @@ let rec dernier = function
     | [] -> failwith "Pas de dernier élément"
     | [e] -> e
     | _::q -> dernier q
+
+let premier n =
+    let rec aux d = (* teste s'il existe un d < n divisant n *)
+        if d = 1 then false (* on n'a pas trouvé de diviseur *)
+        else n mod d = 0 || aux (d - 1) in
+    aux (n/2)
+    
+    let tous_premiers n =
+    if n = 1 then []
+    else let l = tous_premiers (n - 1) in
+         if premier n then n::l
+         else l 
+        
+    let rec bezout a b =
+    if b = 0 then (a, 1, 1)  (* a = 1*a + 1*b *)
+    else let d, u', v' = bezout b (a mod b) in
+         d, v', u' - v'*(a/b) in
+
+let rec fusion l1 l2 = match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | e1::q1, e2::q2 -> if e1 < e2 then e1::fusion q1 l2
+                        else e2::fusion l1 q2 in
+fusion [2; 5; 8] [3; 4; 10];;
+
+let rec kfusion ll = match ll with
+    | [] -> []
+    | l::q -> fusion l (kfusion q) in
+kfusion [[2; 5; 8]; [3; 4; 10]; [0; 13]];;
+
